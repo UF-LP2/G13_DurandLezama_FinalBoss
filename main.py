8d4de1fbb940181470f2d73a6482362ed8944297
saulezama <113647811+saulezama@users.noreply.github.com>
2023-08-14 20:35:35 -0300

    falta unit test

--- main.py
+++ main.py
@@ -5,32 +5,42 @@ from src.cFinalBoss import Cargo
 from src.cFinalBoss import Ship
 from src.cFinalBoss import Cruise
 
+
 def main() -> None:
-  """Function printing python version."""
-  barcos=[]
-  with open("src/ships.csv",'read') as file:
-    reader=csv.reader(file)
-    for row in reader:
-      draft=float(row['cargo'])
-      crew=float(row['crew'])
-      extra=float(row['extra'])
-      quality=float(row['quality'])
-      
-      if quality=="" and extra=="":
-        ship_obj=Ship(draft,crew)
-        barcos.append(ship_obj)
-      elif quality !="":
-        cargo_obj=Cargo(extra,quality,draft,crew)
-        barcos.append(cargo_obj)
-      elif quality =="":
-        crusero_obj=Cruise(extra,draft,crew)
-        barcos.append(crusero_obj)
-  
-  for i in barcos:
-    try:
-      i.is_worth_it()
-    except ValueError as error:
-      print(error)
+    """Function printing python version."""
+    barcos = []
+    with open("src/ship.csv") as file:
+        reader = csv.reader(file, delimiter=',')
+        next(file, None)
+
+        for row in reader:
+            draft = row[0]
+            crew = row[1]
+            extra = row[2]
+            quality = row[3]
+
+            if quality == "" and extra == "":
+                ship_obj = Ship(draft, crew)
+                barcos.append(ship_obj)
+
+            elif quality != "":
+                if extra == "":
+                    extra = 0
+
+                cargo_obj = Cargo(extra, quality, draft, crew)
+                barcos.append(cargo_obj)
+            elif quality == "":
+                crusero_obj = Cruise(extra, draft, crew)
+                barcos.append(crusero_obj)
+
+    for i in barcos:
+        try:
+            flag = i.is_worth_it()
+            if flag:
+                print("saquear")
+        except ValueError as error:
+            print(error)
+
 
 if __name__ == "__main__":
-  main()
+    main()
--- src/cFinalBoss.py
+++ src/cFinalBoss.py
@@ -1,54 +1,62 @@
 """ declaracio Clases"""
 
+
 class Ship:
+    """Function printing python version."""
 
     def __init__(self, draft, crew):
-        self.draft = draft
-        self.crew = crew
+        self.draft = float(draft)
+        self.crew = float(crew)
+
     def is_worth_it(self):
-       if(self.draft- self.crew*1.5)<=20:
-            raise Exception("Error, no saquear")
-       elif(self.draft- self.crew*1.5)>20:
-          return True
-       
+        """Function printing python version."""
+        if (self.draft - self.crew*1.5) <= 20:
+            raise ValueError("Error, no saquear")
+        elif (self.draft - self.crew*1.5) > 20:
+            return True
+
+
 class Cargo(Ship):
+    """Function printing python version."""
 
     def __init__(self, cargo, quality, draft, crew):
-     self.cargo = cargo
-     self.quality = quality
-     super().__init__(draft,crew)
+        self.cargo = float(cargo)
+        self.quality = (quality)
+        super().__init__(draft, crew)
 
     def checking_cargo(self):
+        """Function printing python version."""
+        if self.cargo == "":
+            self.cargo = 0
 
-        if self.cargo=="":
-            self.cargo=0
-
-        if self.quality==1:
-            self.cargo= self.cargo+3,5
-        elif self.quality==0.5:
-            self.cargo= self.cargo+2
-        elif self.quality==0.25:
-            self.cargo= self.cargo+0.5
+        if self.quality == 1:
+            self.cargo = self.cargo+3, 5
+        elif self.quality == 0.5:
+            self.cargo = self.cargo+2
+        elif self.quality == 0.25:
+            self.cargo = self.cargo+0.5
         else:
-            raise Exception("Error, no hay cargo")
+            raise ValueError("Error, no hay cargo")
 
     def is_worth_it(self):
-       self.checking_cargo()
+        self.checking_cargo()
 
-       if(self.draft+self.cargo)- self.crew*1.5 <=20:
-          raise Exception("Error, no saquear")
-       elif(self.draft+self.cargo)- self.crew*1.5>20:
-          return True
-  
+        if (self.draft+self.cargo) - self.crew*1.5 <= 20:
+            raise ValueError("Error, no saquear")
+        elif (self.draft+self.cargo) - self.crew*1.5 > 20:
+            return True
+
+
 class Cruise(Ship):
+    """Function printing python version."""
 
     def __init__(self, passengers, draft, crew):
-     self.passengers = passengers
-     super().__init__(draft,crew)
+        self.passengers = float(passengers)
+        super().__init__(draft, crew)
 
     def is_worth_it(self):
 
-       if(self.draft- self.crew*1.5)- self.passengers <=20:
-          raise Exception("Error, no saquear")
-       elif(self.draft- self.crew*1.5)- self.passengers >20:
-          return True
+        if (self.draft - self.crew*1.5) - self.passengers <= 20:
+            raise ValueError("Error, no saquear")
+        elif (self.draft - self.crew*1.5) - self.passengers > 20:
+            return True
